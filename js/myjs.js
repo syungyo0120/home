9e4449307a3e79f4c8ac49144f45d9eff0e2f5da
syungyo0120 <76052003+syungyo0120@users.noreply.github.com>
2021-02-28 18:35:39 +0900

    Add files via upload

---
+++ js/myjs.js
@@ -0,0 +1,9 @@
+function cp(file){
+    var txt = document.getElementById(file);
+    var k = Object.keys(txt);
+    console.log(txt);
+    console.log(typeof(txt));
+    console.log(k);
+    // txt.select();
+    // document.execCommand("Copy");
+ }
